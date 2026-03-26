282125c9b789572f12a0a862268c3c2b4923a91d
pisitpongth <pisitpong242550@gmail.com>
2026-03-27 01:40:45 +0700

    Fixed embedded repository issue

---
+++ C++/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  // This is a comment
+
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << "This is my first time coding C++" << std::endl;
+
+  return 0;
+}
